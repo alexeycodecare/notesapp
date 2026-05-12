c21f2fcd8dec73a9ad5fc016010d98e240ce2357
Alexey <alexey@code-care.pro>
2026-05-12 20:27:01 +0300

    feat: update modelId in generateImage function to use nova-canvas-v2

--- amplify/functions/generateImage/handler.ts
+++ amplify/functions/generateImage/handler.ts
@@ -9,7 +9,7 @@ export const handler = async (event: any) => {
     const { prompt } = event.arguments;
 
     const command = new InvokeModelCommand({
-      modelId: "amazon.titan-image-generator-v1",
+      modelId: "amazon.nova-canvas-v2:0",
       contentType: "application/json",
       accept: "application/json",
       body: JSON.stringify({
